2a327eb854cab55c28beb3b16d08216a9fcaeb7f
Rayanch0 <chabatianis@yahoo.fr>
2024-12-16 17:12:52 +0100

    exercice 2 added

---
+++ exercise 2.py
@@ -0,0 +1,13 @@
+temperature=int(input("Please type in the temperature: "))
+if temperature<0:
+  print("It's freezing!")
+  print("It's cold! \nIt's cool!")
+elif temperature<10:
+  print("It's cold! \nIt's cool!")
+elif temperature<20:
+  print("It's cool!")
+print("Stay safe!")
+
+
+
+
