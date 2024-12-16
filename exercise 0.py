0bc6d3dfd7c06427771ab74e87c92913b70e89a2
Adelchabati <adel.chabati@etu.usthb.dz>
2024-12-16 16:31:34 +0100

    exercice 0 added

---
+++ exercise 0.py
@@ -0,0 +1,6 @@
+people=int(input("how many people needs a ride ? "))
+fit=int(input("how many people fit in one taxi ? "))
+number=people//fit
+if people%fit != 0:
+  number=number+1
+print(f"number of taxi needed {number}")  
